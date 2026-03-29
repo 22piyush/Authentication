423b72217564e15b09a97d43af07a26fcfe909c9
22piyush <piyushaglawe05@gmail.com>
2026-03-29 11:53:10 +0530

    done

--- server/app.js
+++ server/app.js
@@ -1,1 +1,16 @@
 import express from "express";
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+export const app = express();
+dotenv.config();
+const port = process.env.PORT;
+app.use(cors());
+
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
