db231bfaa0f94fb8eb194260866e19afb4f2c928
22piyush <piyushaglawe05@gmail.com>
2026-03-29 12:36:55 +0530

    done

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -48,7 +48,13 @@ userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) {
         next();
     }
+    this.password = await bcrypt.hash(this.password, 10)
 });
+
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword);
+}
 
 
 
